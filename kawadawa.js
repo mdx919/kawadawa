64b31468739c5fa9a3d30bf6ef9398038fbd7235
naim <thanvirdx919@gmail.com>
2017-08-27 02:05:53 -0400

    fire auth is not working, but login.js and reguster.js is made

---
+++ kawadawa.js
@@ -0,0 +1,41 @@
+
+(function(){
+
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyDa5jTw7ySbsPLCYJDgHh5pIBkCwFjwlXs",
+        authDomain: "footpath-f976a.firebaseapp.com",
+        databaseURL: "https://footpath-f976a.firebaseio.com",
+        projectId: "footpath-f976a",
+        storageBucket: "footpath-f976a.appspot.com",
+        messagingSenderId: "39007593753"
+      };
+      firebase.initializeApp(config);
+
+
+      const textEmail = document.getElementById('email');
+      const textPassword = document.getElementById('password');
+      const btnLogin = document.getElementById('login');
+
+
+      btnLogin.addEventListener('click', e => {
+
+        const email = textEmail.value;
+        const password = textPassword.value;
+        const auth = firebase.auth();
+
+        // sign in
+        const promise = auth().signInWithEmailAndPassword(email, password);
+
+        // Handle Errors here.
+        promise.catch(e => alert(e.message));
+  
+      });
+
+      })
+
+
+
+
+
+};
